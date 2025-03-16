3f753e129a2d98da28367f565c4b9cd847f1af7b
MaximaCaceres <maximavanecaceres@gmail.com>
2025-03-15 22:00:43 -0300

    Update

--- WebApplication1/WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WebApplication1/WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee18183460316ad7c30d60be55e20ad00f78a302")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+918e5dcf33de87cdf66b1b14a5e2650fb5a07a53")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
